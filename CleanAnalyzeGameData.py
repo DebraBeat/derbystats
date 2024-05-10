61db5ebbcaf66e43933219744b61e9323e60100b
Debra Ritter <zeeritter@gmail.com>
2024-05-10 14:26:18 -0400

    Finished creating an Elo DataFrame, and refactored the creation of the Elo DataFrame to be a part of an Elo class, which will be updated in the future with corresponding methods.

--- CleanAnalyzeGameData.py
+++ CleanAnalyzeGameData.py
@@ -21,12 +21,36 @@ df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='coerce')
 primary_scores_df = df[['Home Team', 'Away Team', 'Date', 'Home Team Score', 'Away Team Score']].dropna()
 # Sort by date
 primary_scores_df = primary_scores_df.sort_values(by=['Date'])
-# ELO Ranking System
-# First we create a DataFrame for each team and their ELO Score
-elo_df1 = pd.DataFrame(primary_scores_df['Home Team'])
-elo_df2 = pd.DataFrame(primary_scores_df['Away Team'])
 
-elo_df1.rename({'Home Team': 'Team'})
-elo_df2.rename({'Away Team': 'Team'})
+
+# ELO Ranking System
+
+
+# Now we need two methods: one for expected score, and the other for updating ranking
+# Expected score method
+class Elo:
+    def __init__(self):
+        # Number comes from wikipedia, could be changed later
+        k = 32
+        # Create a DataFrame for each team and their ELO Score
+        self.elo_df = pd.DataFrame()
+
+    def create_elo_df(self, df: pd.DataFrame, home_team_col: str, away_team_col: str) -> pd.DataFrame():
+        # Concatenate the team columns from the main df into a Series
+        self.elo_df = pd.concat([df[home_team_col], df[away_team_col]])
+        # Drop duplicates from the Series
+        self.elo_df = self.elo_df.drop_duplicates()
+        # Convert the Series to a DataFrame
+        self.elo_df = self.elo_df.to_frame()
+        # Name the column (which was previously unnamed)
+        self.elo_df.columns = ['Team']
+        # Insert another column for the score, initialized to 1200
+        self.elo_df.insert(1, "Elo Score", 1200)
+
+        return self.elo_df
+
+    def expected_scores(self, home_team_name: str, away_team_name: str) -> [int]:
+        # self.elo_df.loc['']
+        pass
 # Save the primary scores df for later use
 primary_scores_df.to_csv('PrimaryScoresData')

059edfa05a96906dae719f66fdb68e758b784595
Debra Ritter <zeeritter@gmail.com>
2024-05-19 20:13:55 -0400

    Added more to the glicko class.

--- AnalyzeGameData.py
+++ AnalyzeGameData.py
@@ -2,6 +2,7 @@ import math
 import pandas as pd
 from CleanDataframe import clean
 
+# TODO: Convert some comments to docstrings.
 df = pd.read_csv('GameData.csv')
 
 df = clean(df)
@@ -15,10 +16,13 @@ primary_scores_df = primary_scores_df.sort_values(by=['Date'])
 # ELO Ranking System
 class Elo:
     def __init__(self, input_df, home_team_col, away_team_col):
-        # Number is a guess based on flat track stats data
-        # In the initial ELO implementation, k = 32 and exp / 400, since flat track stats uses exp / 100,
-        # I bumped k up by 4
-        # What needs to be implemented is a k function that takes in total number of games played
+        """
+        Number is a guess based on flat track stats data
+        In the initial ELO implementation, k = 32 and exp / 400, since flat track stats uses exp / 100,
+        I bumped k up by 4
+        What needs to be implemented is a k function that takes in total number of games played
+        """
+
         self.k = 128
         # Number comes from flat track stats
         self.delta = 0.06
@@ -73,19 +77,25 @@ class Elo:
 # Implementation of this document: http://www.glicko.net/glicko/glicko2.pdf
 class Glicko2:
     def __init__(self, input_df, home_team_col, away_team_col):
-        self._tau = 0.75
+        self.__tau = 0.75
+        self.__epsilon = 0.000001
 
+        # Create a DataFrame consisting of the team names in the same fashion as the Elo class.
         self.glicko_df = pd.concat(input_df[home_team_col], input_df[away_team_col])
         self.glicko_df = self.glicko_df.drop_duplicates()
         self.glicko_df = self.glicko_df.to_frame()
         self.glicko_df.columns = ['Team']
 
+        # Create and populate the columns of the glicko_df
         self.glicko_df.insert(1, 'Glicko-2 Rating', 1500)
         self.glicko_df.insert(2, 'Rating Deviation', 350)
         self.glicko_df.insert(3, 'Rating Volatility', 0.06)
-        self.glicko_df.insert(4, 'V Value')
+        self.glicko_df.insert(4, 'V', 0)
+        self.glicko_df.insert(5, 'Delta', 0)
 
+        # Rename rows to the values in the first column (which are the team names)
         self.glicko_df = self.glicko_df.set_axis(self.glicko_df['Team'], axis=0)
+        # Drop the first column
         self.glicko_df = self.glicko_df.drop(columns='Team')
 
     # mu is the rating normalized to the glicko2 scale
@@ -98,15 +108,48 @@ class Glicko2:
         team_rd = self.glicko_df.loc[team_name, 'Rating Deviation']
         return team_rd / 173.7178
 
+    def get_g(self, home_team_name):
+         return 1.0 / math.sqrt(1 + 3 * self.get_phi(home_team_name)**2 / math.pi**2)
+
+    def get_e(self, home_team_name, away_team_name):
+        g = self.get_g(home_team_name)
+        return 1.0 / (1 + math.exp(-1.0 * g * (self.get_mu(home_team_name) - self.get_mu(away_team_name))))
     def get_v(self, home_team_name, away_team_name):
-        team_v = self.glicko_df.loc[home_team_name, 'V Value']
-        g = 1.0 / math.sqrt(1 + 3 * self.get_phi(home_team_name)**2 / math.pi**2)
-        e = 1.0 / (1 + math.exp(-1.0 * g * (self.get_mu(home_team_name) - self.get_mu(away_team_name))))
+        team_v = self.glicko_df.loc[home_team_name, 'V']
+        g = self.get_g(home_team_name)
+        e = self.get_e(home_team_name, away_team_name)
 
         return (team_v + g**2 * e * (1 - e))**-1
 
-    def get_delta(self, team_name):
-        pass
+    def get_delta(self, s, home_team_name, away_team_name):
+        team_v = self.glicko_df.loc[home_team_name, 'V']
+        team_delta = self.glicko_df.loc[home_team_name, 'Delta']
+        g = self.get_g(home_team_name)
+        e = self.get_e(home_team_name, away_team_name)
+
+        return team_v * (team_delta + (g * (s - e)))
+
+    def get_new_vol(self, home_team_name, away_team_name, home_team_score, away_team_score):
+        sigma = self.glicko_df.loc[home_team_name, 'Rating Volatility']
+        phi = self.get_phi(home_team_name)
+        v = self.get_v(home_team_name, away_team_name)
+
+        if home_team_score > away_team_score:
+            s = 1
+        elif home_team_score == away_team_score:
+            s = 0.5
+        else:
+            s = 0
+        delta = self.get_delta(s, home_team_name, away_team_name)
+
+        a = math.log(sigma**2)
+
+        A = a
+        if delta**2 > phi**2 + v:
+            B = math.log(delta**2 - phi**2 - v)
+        else:
+            k = 1
+             
 
 elo_instance = Elo(df, 'Home Team', 'Away Team')
 
